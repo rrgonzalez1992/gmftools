0b5cf6d81bb6b0ff6b0b5503d4e8acef61c3decb
jan.koehnlein <jan.koehnlein@5a3c619e-8ec9-11dd-8d41-03ea52202348>
2008-10-01 08:39:41 +0000

    initial checkin

---
+++ trunk/plugins/de.itemis.gmf.runtime.extensions/src/de/itemis/gmf/runtime/layout/CustomLayout.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2008 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package de.itemis.gmf.runtime.layout;
+
+import org.eclipse.draw2d.ToolbarLayout;
+import org.eclipse.gmf.runtime.draw2d.ui.figures.ConstrainedToolbarLayout;
+
+public class CustomLayout extends ConstrainedToolbarLayout {
+
+	public CustomLayout() {
+		setStretchMinorAxis(true);
+		setStretchMajorAxis(true);
+		setMinorAlignment(ToolbarLayout.ALIGN_TOPLEFT);
+		setSpacing(3);
+		setVertical(true);
+	}
+
+}

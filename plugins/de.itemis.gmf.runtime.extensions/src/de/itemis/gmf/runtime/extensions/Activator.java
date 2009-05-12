bbf5f759f1ddd285b0fee51b433b2564149ed107
jan.koehnlein <jan.koehnlein@5a3c619e-8ec9-11dd-8d41-03ea52202348>
2009-05-12 14:16:59 +0000

    merged 3.4 and 3.5 version of templates

--- plugins/de.itemis.gmf.runtime.extensions/src/de/itemis/gmf/runtime/extensions/Activator.java
+++ plugins/de.itemis.gmf.runtime.extensions/src/de/itemis/gmf/runtime/extensions/Activator.java
@@ -7,6 +7,8 @@
  *******************************************************************************/
 package de.itemis.gmf.runtime.extensions;
 
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -16,11 +18,11 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "de.itemis.gmf.runtime.extensions";
+	public static final String PLUGIN_ID = "de.nordlbit.baseone.gmf.runtime.extensions";
 
 	// The shared instance
 	private static Activator plugin;
-	
+
 	/**
 	 * The constructor
 	 */
@@ -29,7 +31,10 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
@@ -38,7 +43,10 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
@@ -47,11 +55,16 @@ public class Activator extends AbstractUIPlugin {
 
 	/**
 	 * Returns the shared instance
-	 *
+	 * 
 	 * @return the shared instance
 	 */
 	public static Activator getDefault() {
 		return plugin;
+	}
+
+	public static void logError(String message, Throwable exc) {
+		getDefault().getLog().log(
+				new Status(IStatus.ERROR, Activator.PLUGIN_ID, message, exc));
 	}
 
 }

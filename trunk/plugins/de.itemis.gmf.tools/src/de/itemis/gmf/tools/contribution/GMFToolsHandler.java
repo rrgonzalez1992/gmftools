d5b919be8aca33067bcb7090e1fd6894b9b54259
robert.wloch@itemis.de <robert.wloch@itemis.de@5a3c619e-8ec9-11dd-8d41-03ea52202348>
2009-05-29 09:26:42 +0000

    Factored out runnable which encapsulates the overall generation process.

--- trunk/plugins/de.itemis.gmf.tools/src/de/itemis/gmf/tools/contribution/GMFToolsHandler.java
+++ trunk/plugins/de.itemis.gmf.tools/src/de/itemis/gmf/tools/contribution/GMFToolsHandler.java
@@ -7,20 +7,14 @@
  *******************************************************************************/
 package de.itemis.gmf.tools.contribution;
 
-import java.lang.reflect.InvocationTargetException;
-import java.util.HashSet;
-import java.util.Set;
-
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.dialogs.ProgressMonitorDialog;
-import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IWorkbenchWindow;
@@ -30,20 +24,16 @@ import de.itemis.gmf.tools.Activator;
 import de.itemis.gmf.tools.preferences.GmfModel;
 import de.itemis.gmf.tools.preferences.PreferenceUtil;
 
-public class GMFToolsHandler extends AbstractHandler implements
-		IRunnableWithProgress {
+public class GMFToolsHandler extends AbstractHandler {
 
 	private GmfModel lastGmfModel;
 
 	private IWorkbenchWindow window;
 
-	private boolean isOK;
-
-	private Set<IFile> changedGmfGenModels;
-
 	private GmfModel gmfModel;
 
 	public GMFToolsHandler() {
+	
 	}
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
@@ -52,71 +42,18 @@ public class GMFToolsHandler extends AbstractHandler implements
 		if (gmfModel != null) {
 			try {
 				new ProgressMonitorDialog(window.getShell()).run(false, true,
-						this);
-				if (isOK) {
-					for (IFile gmfGenModel : changedGmfGenModels) {
-						GmfDiagramCodeGenerator
-								.generateDiagramCode(gmfGenModel);
-					}
-				}
+						new GMFToolsGeneration(gmfModel));
 			} catch (Exception e) {
 				reportError(e);
 			}
 		} else {
-			MessageDialog.openInformation(window.getShell(),
-					"No GMF file set selected",
-					"Go to Preferences -> GMF Tools and create a set of files for the editor first.");
+			MessageDialog
+					.openInformation(
+							window.getShell(),
+							"No GMF file set selected",
+							"Go to Preferences -> GMF Tools and create a set of files for the editor first.");
 		}
 		return null;
-	}
-
-	public void run(IProgressMonitor monitor) throws InvocationTargetException,
-			InterruptedException {
-		try {
-			changedGmfGenModels = new HashSet<IFile>();
-			isOK = false;
-			monitor.beginTask("Creating transformed GMF generator model", 5);
-			if (PreferenceUtil.isTransformMap2GmfGen()) {
-				isOK = GmfMappingToGenModelTransformer
-						.transformMapToGmfGenModel(gmfModel, monitor);
-				if (!isOK) {
-					return;
-				}
-				if (!PreferenceUtil.isTransformGmfGen()) {
-					changedGmfGenModels.add(gmfModel.getGmfGenModelFile());
-				}
-			}
-			if (PreferenceUtil.isTransformGmfGen()) {
-				isOK = GmfGenModelTransformer.createOrGetTransformationFile(
-						gmfModel, monitor);
-				if (!isOK) {
-					return;
-				}
-				isOK = GmfGenModelTransformer.transformGmfGenModelFile(
-						gmfModel, monitor);
-				if (!isOK) {
-					return;
-				}
-				changedGmfGenModels.add(gmfModel.getTransformedGmfGenModelFile());
-			}
-			if (PreferenceUtil.isFixTypeRegistration()) {
-				isOK = GmfGenModelTypeRegistryHarmonizer
-						.harmonizeTypeRegistration(PreferenceUtil
-								.getGmfModels(), changedGmfGenModels,
-								monitor);
-				if (!isOK) {
-					return;
-				}
-			}
-			if (PreferenceUtil.isDeleteDiagramPlugin()
-					&& !changedGmfGenModels.isEmpty()) {
-				isOK = GmfGeneratedPluginRemover.deleteGeneratedPlugins(
-						changedGmfGenModels, monitor);
-			}
-		} catch (Exception exc) {
-			reportError(exc);
-			isOK = false;
-		}
 	}
 
 	private void reportError(Exception e) {
@@ -136,7 +73,7 @@ public class GMFToolsHandler extends AbstractHandler implements
 					for (GmfModel gmfModels : PreferenceUtil.getGmfModels()) {
 						if (gmfModels.hasFile((IFile) selectedElement))
 							lastGmfModel = gmfModels;
-							return gmfModels;
+						return gmfModels;
 					}
 				}
 

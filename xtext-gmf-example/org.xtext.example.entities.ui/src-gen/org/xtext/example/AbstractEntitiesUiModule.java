568755c1d08f336e1defbcf61b546c1b55db9fbe
jan.koehnlein <jan.koehnlein@5a3c619e-8ec9-11dd-8d41-03ea52202348>
2009-12-04 13:27:14 +0000

    Updated to HEAD

--- xtext-gmf-example/org.xtext.example.entities.ui/src-gen/org/xtext/example/AbstractEntitiesUiModule.java
+++ xtext-gmf-example/org.xtext.example.entities.ui/src-gen/org/xtext/example/AbstractEntitiesUiModule.java
@@ -108,11 +108,6 @@ public abstract class AbstractEntitiesUiModule extends EntitiesRuntimeModule {
 	}
 
 	// contributed by org.eclipse.xtext.ui.generator.ImplicitUiFragment
-	public Class<? extends org.eclipse.xtext.ui.core.editor.validation.IXtextResourceChecker> bindIXtextResourceChecker() {
-		return org.eclipse.xtext.ui.core.editor.validation.DefaultXtextResourceChecker.class;
-	}
-
-	// contributed by org.eclipse.xtext.ui.generator.ImplicitUiFragment
 	public Class<? extends com.google.inject.Provider<org.eclipse.xtext.index.IXtextIndex>> provideIXtextIndex() {
 		return org.eclipse.xtext.ui.core.index.IndexProvider.class;
 	}
@@ -123,18 +118,18 @@ public abstract class AbstractEntitiesUiModule extends EntitiesRuntimeModule {
 	}
 
 	// contributed by org.eclipse.xtext.ui.generator.ImplicitUiFragment
-	public com.google.inject.Provider<org.eclipse.xtext.resource.IExternalContentSupport.IExternalContentProvider> provideIExternalContentSupport$IExternalContentProvider() {
-		return org.eclipse.xtext.ui.core.editor.IDirtyStateManager.PROVIDER_AS_CONTENTPROVIDER;
+	public Class<? extends org.eclipse.xtext.resource.IExternalContentSupport.IExternalContentProvider> bindIExternalContentSupport$IExternalContentProvider() {
+		return org.eclipse.xtext.ui.core.editor.IDirtyStateManager.class;
 	}
 
 	// contributed by org.eclipse.xtext.ui.generator.ImplicitUiFragment
 	public com.google.inject.Provider<org.eclipse.xtext.ui.core.editor.IDirtyStateManager> provideIDirtyStateManager() {
-		return org.eclipse.xtext.ui.core.editor.IDirtyStateManager.PROVIDER;
+		return new org.eclipse.xtext.ui.core.editor.DirtyStateManagerProvider();
 	}
 
 	// contributed by org.eclipse.xtext.ui.generator.ImplicitUiFragment
 	public com.google.inject.Provider<org.eclipse.xtext.ui.core.notification.IStateChangeEventBroker> provideIStateChangeEventBroker() {
-		return org.eclipse.xtext.ui.core.notification.IStateChangeEventBroker.PROVIDER;
+		return new org.eclipse.xtext.ui.core.notification.StateChangeEventBrokerProvider();
 	}
 
 	// contributed by de.itemis.xtext.antlr.XtextAntlrGeneratorFragment

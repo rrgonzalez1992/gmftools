d5cf6ca11360badf37ee770215f943a0c17843e5
jan.koehnlein <jan.koehnlein@5a3c619e-8ec9-11dd-8d41-03ea52202348>
2009-12-08 14:26:50 +0000

    property view reconciling

--- trunk/xtext-gmf-example/org.eclipse.xtext.gmf.glue/src/org/eclipse/xtext/gmf/glue/editingdomain/XtextNodeModelReconciler.java
+++ trunk/xtext-gmf-example/org.eclipse.xtext.gmf.glue/src/org/eclipse/xtext/gmf/glue/editingdomain/XtextNodeModelReconciler.java
@@ -1,19 +1,20 @@
 package org.eclipse.xtext.gmf.glue.editingdomain;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.commands.operations.IOperationHistory;
+import org.eclipse.core.commands.operations.IOperationHistoryListener;
+import org.eclipse.core.commands.operations.OperationHistoryEvent;
+import org.eclipse.emf.common.command.CommandStack;
 import org.eclipse.emf.common.notify.impl.AdapterImpl;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.EcoreUtil;
-import org.eclipse.emf.transaction.Transaction;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.emf.transaction.TransactionalEditingDomainEvent;
 import org.eclipse.emf.transaction.TransactionalEditingDomainListener;
 import org.eclipse.emf.transaction.TransactionalEditingDomain.Lifecycle;
 import org.eclipse.emf.transaction.util.TransactionUtil;
+import org.eclipse.emf.workspace.IWorkspaceCommandStack;
+import org.eclipse.gmf.runtime.common.core.command.ICommand;
 import org.eclipse.xtext.gmf.glue.Activator;
 import org.eclipse.xtext.parsetree.CompositeNode;
 import org.eclipse.xtext.parsetree.NodeAdapter;
@@ -28,16 +29,11 @@ import org.eclipse.xtext.resource.XtextResource;
  * @author koehnlein
  */
 public class XtextNodeModelReconciler extends AdapterImpl implements
-		TransactionalEditingDomainListener {
+		TransactionalEditingDomainListener, IOperationHistoryListener {
 
 	private TransactionalEditingDomain editingDomain;
 
 	private ChangeAggregatorAdapter changeAggregator;
-
-	// Oh my god: GMF opens another root transaction for the post commit
-	// listeners before closing the last one. As no changes are neither
-	// recorded nor rolled back in this transaction, we just ignore it
-	private List<Transaction> openTransactions = new ArrayList<Transaction>();
 
 	private XtextNodeModelReconciler(TransactionalEditingDomain editingDomain) {
 		this.editingDomain = editingDomain;
@@ -46,10 +42,23 @@ public class XtextNodeModelReconciler extends AdapterImpl implements
 		lifecycle.addTransactionalEditingDomainListener(this);
 		changeAggregator = new ChangeAggregatorAdapter();
 		editingDomain.getResourceSet().eAdapters().add(changeAggregator);
+		CommandStack commandStack = editingDomain.getCommandStack();
+		if (commandStack instanceof IWorkspaceCommandStack) {
+			IOperationHistory operationHistory = ((IWorkspaceCommandStack) commandStack)
+					.getOperationHistory();
+			operationHistory.addOperationHistoryListener(this);
+		}
+		changeAggregator.beginRecording();
 	}
 
 	public void editingDomainDisposing(TransactionalEditingDomainEvent event) {
 		changeAggregator.endRecording();
+		CommandStack commandStack = editingDomain.getCommandStack();
+		if (commandStack instanceof IWorkspaceCommandStack) {
+			IOperationHistory operationHistory = ((IWorkspaceCommandStack) commandStack)
+					.getOperationHistory();
+			operationHistory.removeOperationHistoryListener(this);
+		}
 		editingDomain.getResourceSet().eAdapters().remove(changeAggregator);
 		Lifecycle lifecycle = TransactionUtil.getAdapter(editingDomain,
 				Lifecycle.class);
@@ -58,32 +67,7 @@ public class XtextNodeModelReconciler extends AdapterImpl implements
 	}
 
 	public void transactionClosed(TransactionalEditingDomainEvent event) {
-		openTransactions.remove(openTransactions.size() - 1);
-		if (openTransactions.isEmpty()) {
-			changeAggregator.endRecording();
-			IStatus status = event.getTransaction().getStatus();
-			if (status.isOK()) {
-				for (EObject modificationRoot : changeAggregator
-						.getModificationRoots()) {
-					try {
-						XtextResource xtextResource = (XtextResource) modificationRoot
-								.eResource();
-						NodeAdapter nodeAdapter = NodeUtil
-								.getNodeAdapter(modificationRoot);
-						CompositeNode parserNode = nodeAdapter.getParserNode();
-						SerializerUtil serializer = xtextResource
-								.getSerializer();
-						String newText = serializer.serialize(modificationRoot);
-						UpdateXtextResourceTextCommand.createICommand(
-								xtextResource, parserNode.getOffset(),
-								parserNode.getLength(), newText).execute(null,
-								null);
-					} catch (ExecutionException exc) {
-						Activator.logError(exc);
-					}
-				}
-			}
-		}
+		// ignore
 	}
 
 	public void transactionClosing(TransactionalEditingDomainEvent event) {
@@ -91,17 +75,11 @@ public class XtextNodeModelReconciler extends AdapterImpl implements
 	}
 
 	public void transactionInterrupted(TransactionalEditingDomainEvent event) {
-		openTransactions.remove(openTransactions.size() - 1);
-		if (openTransactions.isEmpty()) {
-			changeAggregator.endRecording();
-		}
+		// ignore
 	}
 
 	public void transactionStarted(TransactionalEditingDomainEvent event) {
-		openTransactions.add(event.getTransaction());
-		if (openTransactions.size() == 1) {
-			changeAggregator.beginRecording();
-		}
+		// ignore
 	}
 
 	public void transactionStarting(TransactionalEditingDomainEvent event) {
@@ -119,4 +97,44 @@ public class XtextNodeModelReconciler extends AdapterImpl implements
 		return adapter;
 	}
 
+	public void historyNotification(OperationHistoryEvent event) {
+		int eventType = event.getEventType();
+		switch (eventType) {
+		case OperationHistoryEvent.DONE:
+		case OperationHistoryEvent.UNDONE:
+		case OperationHistoryEvent.REDONE:
+			changeAggregator.endRecording();
+			ICommand updateXtextResourceTextCommand = null;
+			for (EObject modificationRoot : changeAggregator
+					.getModificationRoots()) {
+				XtextResource xtextResource = (XtextResource) modificationRoot
+						.eResource();
+				NodeAdapter nodeAdapter = NodeUtil
+						.getNodeAdapter(modificationRoot);
+				CompositeNode parserNode = nodeAdapter.getParserNode();
+				SerializerUtil serializer = xtextResource.getSerializer();
+				String newText = serializer.serialize(modificationRoot);
+				ICommand newCommand = UpdateXtextResourceTextCommand
+						.createICommand(xtextResource, parserNode.getOffset(),
+								parserNode.getLength(), newText);
+				if (updateXtextResourceTextCommand == null) {
+					updateXtextResourceTextCommand = newCommand;
+				} else {
+					updateXtextResourceTextCommand.compose(newCommand);
+				}
+			}
+			try {
+				if (updateXtextResourceTextCommand != null) {
+					updateXtextResourceTextCommand.execute(null, null);
+				}
+			} catch (ExecutionException exc) {
+				Activator.logError(exc);
+			}
+			changeAggregator.beginRecording();
+			break;
+		default:
+			// ignore
+		}
+
+	}
 }
